b6813f0a47658d9e00f650aa9a46c0f2b79d7317
Nicolas RAMOUSSE <165056224+nrl-ubp@users.noreply.github.com>
2026-09-09 11:21:28 +0200

    Update TestProfile.java

--- src/test/java/com/ubp/rgd/proxy/services/TestProfile.java
+++ src/test/java/com/ubp/rgd/proxy/services/TestProfile.java
@@ -15,7 +15,7 @@ public class TestProfile implements QuarkusTestProfile {
 
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Map.of(
+        Map<String, String> props = new java.util.HashMap<>(Map.of(
                 "proxy.target.base-url", "http://localhost:37200",
                 "quarkus.log.console.json.enabled", "false",
                 "proxy.prefilter.auth-enabled", "false",
@@ -23,7 +23,13 @@ public class TestProfile implements QuarkusTestProfile {
                 "proxy.transform.endpoint.authorized-spn", "HTTP/dcle01.corp.ubp.ch",
                 "proxy.transform.endpoint.right-context-target", "WDX1",
                 "proxy.transform.endpoint.right-context-module", "WDX1Proxy",
-                "proxy.transform.endpoint.right-context-right", "Transform"
-        );
+                "proxy.transform.endpoint.right-context-right", "Transform",
+                "proxy.flight-sql.jdbc-url", "jdbc:jtds:sqlserver://msvgvzwdxev004s/CDM_Datamart;encrypt=true;trustServerCertificate=true",
+                "proxy.flight-sql.enabled", "true"
+        ));
+
+        props.put("proxy.transform.config-file", "./src/test/resources/rps_transform_config.json");
+
+        return props;
     }
 }
