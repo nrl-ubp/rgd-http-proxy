3f5fbd54db4d0ae6837d55295b248227bb766cda
Nicolas RAMOUSSE <165056224+nrl-ubp@users.noreply.github.com>
2026-09-09 11:12:18 +0200

    Update EntityTransformConfig.java

--- src/main/java/com/ubp/rgd/proxy/transform/config/EntityTransformConfig.java
+++ src/main/java/com/ubp/rgd/proxy/transform/config/EntityTransformConfig.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class EntityTransformConfig extends AbstractTransformConfig {
+    public static final String DATE_REGEXP =
+            "^(?:(?<ymd>\\d{4}-\\d{2}-\\d{2})(?:T(?<hms>\\d{2}:\\d{2}:\\d{2}))?|(?<dmyslash>\\d{2}/\\d{2}/\\d{4})|(?<dmycompact>\\d{2}\\d{2}\\d{4})|(?<dmydash>\\d{2}-\\d{2}-\\d{4}))$";
 
     @JsonProperty(value = "json-path")
     private String jsonPath = "";
