1582547894623eb00c588158c584ac9b5041cc0b
Nicolas RAMOUSSE <165056224+nrl-ubp@users.noreply.github.com>
2026-09-17 15:24:34 +0200

    Update TestProfile.java

--- src/test/java/com/ubp/rgd/proxy/services/TestProfile.java
+++ src/test/java/com/ubp/rgd/proxy/services/TestProfile.java
@@ -33,8 +33,6 @@ public class TestProfile implements QuarkusTestProfile {
         props.putAll(Map.of("proxy.file-transform.enabled","true",
                             "proxy.file-transform.config-file","./src/test/resources/file_transform_config.json"));
 
-        props.put("proxy.transform.impl", "FPE");
-
         return props;
     }
 }
